01b759e4f29fa029501db697fd866c0c4d9b5de9
Lars Buitinck <larsmans@gmail.com>
2013-08-01 23:08:30 +0200

    ENH add setup.py script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="seqlearn",
+    version="0.0.0",
+    description="Sequence learning toolkit",
+    maintainer="Lars Buitinck",
+    maintainer_email="L.J.Buitinck@uva.nl",
+    license="MIT",
+    url="https://github.com/larsmans/seqlearn",
+    packages=["seqlearn"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Text Processing",
+    ],
+)
