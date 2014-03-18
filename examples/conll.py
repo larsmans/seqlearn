e3fc366710eff39d3b8651868510ac9e417865d6
Lars Buitinck <larsmans@gmail.com>
2014-03-18 16:38:12 +0100

    ENH speed up/simplify feature extraction in example

--- examples/conll.py
+++ examples/conll.py
@@ -22,15 +22,15 @@ def features(sentence, i):
 
     word = sentence[i]
 
-    yield "word:{}".format(word.lower())
+    yield "word:{}" + word.lower()
 
     if word[0].isupper():
         yield "CAP"
 
     if i > 0:
-        yield "word-1:{}".format(sentence[i - 1].lower())
+        yield "word-1:{}" + sentence[i - 1].lower()
     if i + 1 < len(sentence):
-        yield "word+1:{}".format(sentence[i + 1].lower())
+        yield "word+1:{}" + sentence[i + 1].lower()
 
 
 def describe(X, lengths):
