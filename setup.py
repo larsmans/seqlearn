d38d2dc11c4348d53df40ffebf2100f6af049aa6
Lars Buitinck <larsmans@gmail.com>
2014-04-06 18:33:15 +0200

    ENH version 0.1: improved setup script, __version__

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os.path
 import sys
+
+
+dist_dir = os.path.dirname(os.path.abspath(__file__))
+execfile(os.path.join(dist_dir, 'seqlearn/_version.py'))
+
+
+def readme():
+    try:
+        with open(os.path.join(dist_dir, 'README.rst')) as f:
+            return f.read()
+    except IOError:
+        return "seqlearn: sequence classification library for Python"
+
 
 setup_options = dict(
     name="seqlearn",
-    version="0.0.0",
+    version=__version__,
     description="Sequence learning toolkit",
     maintainer="Lars Buitinck",
-    maintainer_email="L.J.Buitinck@uva.nl",
+    maintainer_email="larsmans@gmail.com",
     license="MIT",
     url="https://github.com/larsmans/seqlearn",
     packages=["seqlearn", "seqlearn._utils", "seqlearn._decode", "seqlearn.datasets"],
@@ -33,8 +47,8 @@ setup_options = dict(
 # For these actions, NumPy is not required. We want them to succeed without,
 # for example when pip is used to install seqlearn without NumPy present.
 NO_NUMPY_ACTIONS = ('--help-commands', 'egg_info', '--version', 'clean')
-if not (len(sys.argv) >= 2 and ('--help' in sys.argv[1:]
-                                or sys.argv[1] in NO_NUMPY_ACTIONS)):
+if not ('--help' in sys.argv[1:]
+        or len(sys.argv) > 1 and sys.argv[1] in NO_NUMPY_ACTIONS):
     import numpy
     setup_options['include_dirs'] = [numpy.get_include()]
 
