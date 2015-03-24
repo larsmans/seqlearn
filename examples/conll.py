bf7fc859a0eb5851009dfef96552107cb49d43ab
Lars Buitinck <larsmans@gmail.com>
2015-03-24 15:45:15 +0100

    make conll.py work on nerdata/
    Had to add empty lines (sentence boundaries) at the end of each file
    to get the F1 score from 0 to ~30%.

--- examples/conll.py
+++ examples/conll.py
@@ -5,6 +5,8 @@ Generic sequence prediction script using CoNLL format.
 """
 
 from __future__ import print_function
+import fileinput
+from glob import glob
 import sys
 
 from seqlearn.datasets import load_conll
@@ -29,28 +31,51 @@ def features(sentence, i):
 
     if i > 0:
         yield "word-1:{}" + sentence[i - 1].lower()
+        if i > 1:
+            yield "word-2:{}" + sentence[i - 2].lower()
     if i + 1 < len(sentence):
         yield "word+1:{}" + sentence[i + 1].lower()
+        if i + 2 < len(sentence):
+            yield "word+2:{}" + sentence[i + 2].lower()
 
 
 def describe(X, lengths):
     print("{0} sequences, {1} tokens.".format(len(lengths), X.shape[0]))
 
 
-if __name__ == "__main__":
-    print(__doc__)
+def load_data():
+    files = glob('nerdata/*.bio')
 
-    if len(sys.argv) < 3:
-        print("Usage: {0} training_file test_file".format(sys.argv[0]))
-        sys.exit(1)
-
+    # 80% training, 20% test
     print("Loading training data...", end=" ")
-    X_train, y_train, lengths_train = load_conll(sys.argv[1], features)
+    train_files = [f for i, f in enumerate(files) if i % 5 != 0]
+    train = load_conll(fileinput.input(train_files), features)
+    X_train, _, lengths_train = train
     describe(X_train, lengths_train)
 
     print("Loading test data...", end=" ")
-    X_test, y_test, lengths_test = load_conll(sys.argv[2], features)
+    test_files = [f for i, f in enumerate(files) if i % 5 == 0]
+    test = load_conll(fileinput.input(test_files), features)
+    X_test, _, lengths_test = test
     describe(X_test, lengths_test)
+
+    return train, test
+
+
+if __name__ == "__main__":
+    print(__doc__)
+
+    #print("Loading training data...", end=" ")
+    #X_train, y_train, lengths_train = load_conll(sys.argv[1], features)
+    #describe(X_train, lengths_train)
+
+    train, test = load_data()
+    X_train, y_train, lengths_train = train
+    X_test, y_test, lengths_test = test
+
+    #print("Loading test data...", end=" ")
+    #X_test, y_test, lengths_test = load_conll(sys.argv[2], features)
+    #describe(X_test, lengths_test)
 
     clf = StructuredPerceptron(verbose=True, max_iter=10)
     print("Training %s" % clf)
